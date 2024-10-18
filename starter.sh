3aef026def86de332d6e21f37d22e65dcec37a57
Daniel <webmaster@ltspiceusers.ch>
2024-10-18 09:02:03 +0200

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-HOME="$(getent passwd $USER | awk -F ':' '{print $6}')"
-cd ${HOME}/kc.linux ; ./kc # Hier ggf. Pfad innerhalb des home Verzeichnisses anpassen !!!
-
+HOME="$(cd -- "$(dirname -- "$0")" && pwd)"
+cd ${HOME} ; ./kc.py
